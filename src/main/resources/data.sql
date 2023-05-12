33d4c21a5e909aeeb3fe5b1fad75003f0292d107
r3ts3l <llopez9047@gmail.com>
2023-05-11 22:18:11 -0400

    adding db schema and load

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Employee (name, role) VALUES ('John Doe', 'Software Engineer');
+INSERT INTO Employee (name, role) VALUES ('Jane Doe', 'Data Scientist');
+INSERT INTO Employee (name, role) VALUES ('Bob Smith', 'Product Manager');
